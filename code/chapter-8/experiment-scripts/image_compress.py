d9a884c67db78ea0086733dda8e6d200a1725f6c
manavmoorthy <manavmoorthy@gmail.com>
2023-09-23 16:28:12 +0530

    chapter 6: - including the download_sample_image function - formatted the code
    chapter 7:
    - no changes
    
    chapter 8:
    - formatted the code
    - no bugs/issues reported
    - did not include colab support, as it was initially not present
    - these scripts are mainly to be configured to invoke API calls by using customized API keys

--- code/chapter-8/experiment-scripts/image_compress.py
+++ code/chapter-8/experiment-scripts/image_compress.py
@@ -1,11 +1,13 @@
 from PIL import Image
 import os
 
+
 def get_file_size(filename):
     stats = os.stat(filename)
     return stats.st_size
 
-filename = 'ObamaSpeech.jpg'
+
+filename = "ObamaSpeech.jpg"
 image = Image.open(filename)
 image = image.rotate(270)
 print((filename, get_file_size(filename)))
@@ -13,6 +15,6 @@ print((filename, get_file_size(filename)))
 quality = 10
 while quality > 5:
     quality -= 5
-    modified_filename = 'Compression/' + filename + '_Quality_' + str(quality) + '.jpg'
-    image.save(modified_filename,format='JPEG',quality=quality)
+    modified_filename = "Compression/" + filename + "_Quality_" + str(quality) + ".jpg"
+    image.save(modified_filename, format="JPEG", quality=quality)
     print((modified_filename, get_file_size(modified_filename)))
